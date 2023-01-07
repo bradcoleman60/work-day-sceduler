5af980b5afc3d19869910339c80602e6286554ad
Brad Coleman <bradcoleman60@gmail.com>
2023-01-06 23:06:31 -0800

    adds DOM traversing elements in Jquery to be used in local storage functions later

--- script.js
+++ script.js
@@ -7,40 +7,68 @@ $(document).ready(function () {
 var currentDate = $.datepicker.formatDate ("DD MM d, yy", new Date());  
 $("#currentDay").text(currentDate);
 
-
 //Iterates for the 8 hours that are displayed on calendar
-for (i=9; i <18; i++){
-
-  let currentHour = dayjs().format("HH");
+for (i=9; i <20; i++){
+  let currentHour = Number(dayjs().format("H"));
   let calendarHour = dayjs().hour(8);
-  
-  //This IF statement sets the current tense based on the current hour of the day
-  if ([i] === currentHour) {
-    var tense = " present"
-  } if([i] < currentHour){
+
+   //This IF statement sets the current tense based on the current hour of the day
+  if ([i] < currentHour) {
     var tense = " past"
-  } else {
+  } else if([i] > currentHour){
     var tense = " future"
+  } else {
+    var tense = " present"
   }
-  //This iterates to show the hours of 9AM to 5PM on the calendar page
+  /*This iterates to show the hours of 9AM to 5PM on the calendar page 
+  the format("hA") property converts the hour from miltary to standard time
+  including an indicator of AM or PM. */
   calendarHour = calendarHour.add([i-8],'hour').format("hA")
 
-
-
-var codeBlock = ("<div id=\"hour-" + [i] +'"'+ " class=\"row time-block" + tense +" \"><div class=\"col-2 col-md-1 hour text-center py-3\">" +calendarHour+ "</div><textarea class=\"col-8 col-md-10 description\" rows=\"3\"> </textarea><button class=\"btn saveBtn col-2 col-md-1\" aria-label=\"save\"><i class=\"fas fa-save\" aria-hidden=\"true\"></i></button></div>");
-
-$(".container-lg").append(codeBlock);
-
-}
-
-
-for (i=9;i<18;i++){
-let calendarHour = dayjs().hour(8);
-calendarHour = calendarHour.add([i-8],'hour').format("hA")
-console.log(calendarHour);
+  /* This code block is interated 9 times and has three variables that change with each 
+  FOR LOOP.  NOTE the use of the escape character of "\" as this string contains HTML with 
+  needed open and close paraentheses*/ 
+  var codeBlock = ("<div id=\"hour-" + [i] +'"'+ " class=\"row time-block" + tense +" \"><div class=\"col-2 col-md-1 hour text-center py-3\">" +calendarHour+ "</div><textarea id=\"text-area-" + [i] + '"' + "class=\"col-8 col-md-10 description\" rows=\"3\"> </textarea><button id=\"button-number-" + [i] + '"' + "class=\"btn saveBtn col-2 col-md-1\" aria-label=\"save\"><i class=\"fas fa-save\" aria-hidden=\"true\"></i></button></div>");
+  
+  /* This appends the CodeBlock to the DIV with the class ".container" and essentially
+  creates the calendar on the page.  NOTE: this did not work if the bootstrap tag of 
+  px-5 is included in the selector*/
+  $(".container-lg").append(codeBlock);
 
 }
 
+// $("#text-area-10").text("this is brad")
+
+// $("#button-number-9").click(function(){
+//     var inputText = $("#text-area-9").val();
+//     console.log(inputText);
+//   })
+
+// console.log($("#hour-8").children('textarea'));
+
+{/* <div class="container-lg px-5">
+      <div id="hour-8" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">8AM</div>
+        <textarea class="col-8 col-md-10 description" rows="3">TESTING</textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      </div>
+    </div> */}
+
+
+//Gets the text that is in the text area of hour-8. 
+var xxx = $("#hour-8").children().eq(1).text()
+console.log("this is it " + xxx)
+
+var parentOfButton = $(".saveBtn").parent().attr('id');
+console.log(parentOfButton);
+
+$("button").on("click", function(){
+  var parentOfButton1 = $(this).parent().attr('id')
+  alert("this button was clicked  " + parentOfButton1)
+
+})
 
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -62,3 +90,5 @@ console.log(calendarHour);
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
